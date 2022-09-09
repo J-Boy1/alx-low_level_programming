908e62928bb353c3b5f91bd46a304058917ee000
Uche Uzondu <uchechukwunedu@gmail.com>
2022-09-08 20:19:46 -0700

    task will require both stdio.h and stdlib.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	grep put(
-		"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-		);
+	write(2,"and that piece of art is useful, - Dora Korpar, 2015-10-19\n",61);
 	return (1);
 }
