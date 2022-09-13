c1ad2fb1c62855ba43c623eb51f089d61ed782e9
Uche Uzondu <uchechukwunedu@gmail.com>
2022-09-13 14:46:29 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  main - entry point
+ *
+ *  return: always 0 (success)
+ */
+int main(void)
+{
+	char p = (putchar);
+
+	putchar (p);
+	putchar ('\n');
+
+	return (0);
+}
