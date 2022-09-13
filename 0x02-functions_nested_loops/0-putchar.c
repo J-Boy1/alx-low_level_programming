ae16b0315cf580f3e7cf0d7d24908b774de57394
Uche Uzondu <uchechukwunedu@gmail.com>
2022-09-13 15:46:32 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * 
  * Return: 0.
  */
 int main(void)
