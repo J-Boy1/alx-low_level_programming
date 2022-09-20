35a7774b400803f93cbedc83c0b922f08948e42b
Uche Uzondu <uchechukwunedu@gmail.com>
2022-09-19 19:37:23 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 
 /**
 * rev_string - a function that reverse a string
@@ -7,16 +7,16 @@
 */
 void rev_string(char *s)
 {
-  int len = 0, i = 0;
-  char aux; 
-  
-  while (s[len] != '\0')
-    len++;
-  
-  while (i < len--)
-  {
-    aux = s[i];
-    s[i++] = s[len];
-    s[len] = aux;
-  }
+	int len = 0, i = 0;
+	char aux;
+	
+	while (s[len] != '\0')
+		len++;
+	
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
 }
