60a24f346d5215c7850eb7810aa9b530c8901dc2
Uche Uzondu <uchechukwunedu@gmail.com>
2022-09-27 14:49:30 -0700

    string string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 int i, j;
 for (i = 0; needle[i] != '\0'; i++)
 {
-for (j = 0; haystack{j] != '\0'; j++)
+for (j = 0; haystack[j] != '\0'; j++)
 {
 if (haystack[j] == needle[i])
 return (needle = 1);
