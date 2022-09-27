806fc139d9efd24a56b8f03d7cac7654dee7b97b
Uche <101931543+J-Boy1@users.noreply.github.com>
2022-09-27 15:20:29 -0700

    string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,15 +8,14 @@
  
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
-for (i = 0; needle[i]; i++)
+ int i, j;
+for (i = 0; needle[i] != '\0'; i++)
 {
-      for (j = 0; haystack[j]; j++)
-      {
-           if (haystack[j] == needle[i])
-               return (needle + i);
-      }
-    
+for (j = 0; haystack[j] != '\0'; j++)
+{
+if (haystack[j] == needle[i])
+return (needle = 1);
+}
 }
 return (0);
 }
