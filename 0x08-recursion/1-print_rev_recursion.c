2580b42202555d6f2069bd3f6f251735d4222670
Uche <101931543+J-Boy1@users.noreply.github.com>
2022-09-28 08:00:48 -0700

    print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,15 @@
 
 /**
  * _print_rev_recursion - a function that prints a string in reverse,
+ * followed by a new line.
  * @s: An input string to printing
  * Return: Nothing
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-if (*s)
-{
-  _print_rev_recursion(s + 1);
-  _putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
-} 
