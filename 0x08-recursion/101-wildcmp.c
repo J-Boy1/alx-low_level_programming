7498a28a7ad8911b0eb39a9d61b65a4976bb9c9c
Uche <101931543+J-Boy1@users.noreply.github.com>
2022-09-28 20:28:54 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,6 +9,11 @@ int wildcmp(char *s1, char *s2)
 {
 	if (!*s1 && !*s2)
 		return (1);
+		if (*s1 =='*')
+		{
+		while (*(*s1 + 1) == '*')
+		*s1++;
+		}
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
